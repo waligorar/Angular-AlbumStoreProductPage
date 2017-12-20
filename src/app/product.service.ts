2744157f90af39f2423713115aa05ce17cc49ef6
rmw195 <rmw195@C02T1209G8WL.local>
2017-12-20 11:13:30 -0500

    part 4 done

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Album } from './album';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
@@ -10,10 +12,10 @@ export class ProductService {
 
   private mapit(res: Response) {
     const body = res.json();
-    return body;
+    return <Album>body;
   }
 
-  getAlbum(id: number) {
+  getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map(this.mapit);
   }
 }
