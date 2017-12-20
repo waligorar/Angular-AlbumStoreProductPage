4b7ffe34d592157d93b9cf38ba2b401bb14c6559
rmw195 <rmw195@C02T1209G8WL.local>
2017-12-20 11:29:21 -0500

    part 5 section a

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -15,6 +15,7 @@ export class ProductTracklistingComponent implements OnInit {
   ngOnInit() {
     this._productService.getAlbum(1).subscribe(
       response => this.albumInfo = response
+    );
   }
 
 }
